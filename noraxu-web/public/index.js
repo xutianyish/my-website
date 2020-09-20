853ee66552c1e4ab67a313f6fe90fc240b13e581
xutianyish <xutianyish@gmail.com>
2020-09-20 16:21:11 -0400

    Add files via upload

---
+++ noraxu-web/public/index.js
@@ -0,0 +1,25 @@
+// Automatic Slideshow - change image every 4 seconds
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 4000);
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
